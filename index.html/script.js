9bef76a3bc3a17f311ba029b3c337f936708b3af
lawrenceokumukabila-netizen <lawrenceokumukabila@gmail.com>
2026-04-29 17:50:56 +0300

    Added projects files

---
+++ index.html/script.js
@@ -0,0 +1,15 @@
+function sendMessage() {
+    let name = 
+    document.getElementById("name").Value;
+
+    if(name === "") 
+        {
+        alert("Dear enter your name");
+    }
+        else
+        {
+            alert("Message sent successfully!");
+        }
+
+}
+
